a00fbb90bb9c4834c6e8aeed4232c5c76e4beb01
Budi Irawan <deerawan@gmail.com>
2017-04-15 19:44:21 +1000

    add variable

---
+++ 03-variable/default-value/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("%v - %T \n", a, a)
+	fmt.Printf("%v - %T \n", b, b)
+	fmt.Printf("%v - %T \n", c, c)
+	fmt.Printf("%v - %T \n", d, d)
+}
---
+++ 03-variable/shorthand/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 10
+	b := "budi"
+	c := 10.50
+	d := false
+
+	fmt.Printf("%v - %T \n", a, a)
+	fmt.Printf("%v - %T \n", b, b)
+	fmt.Printf("%v - %T \n", c, c)
+	fmt.Printf("%v - %T \n", d, d)
+}
