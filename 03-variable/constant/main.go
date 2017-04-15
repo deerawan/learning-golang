cc66913555af3ccfa266050a4b3ee1ed37f21be6
Budi Irawan <deerawan@gmail.com>
2017-04-15 19:54:55 +1000

    add more example variable

---
+++ 03-variable/constant/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const can't be re-assigned
+	const y = "world"
+	fmt.Println(y)
+}
---
+++ 03-variable/declare-many/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 = 1, 2
+	num3, num4 := 3, 4
+
+	var str1, str2 string = "hello", "world"
+
+	fmt.Println(num1, num2, num3, num4)
+	fmt.Println(str1, str2)
+}
